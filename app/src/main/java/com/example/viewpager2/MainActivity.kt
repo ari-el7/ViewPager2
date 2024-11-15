a6547f5be481d3f27baa3c67fcf415a1db9589b4
Wahyudin Yunus <akbar_23510018@stimata.ac.id>
2024-11-15 15:10:34 +0700

    Akbar Atma View Pager 2

---
+++ app/src/main/java/com/example/viewpager2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.viewpager2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        val tablayout = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tablayout, viewPager) {tab, position ->
+            when ( position) {
+                0 -> {
+                    tab.text = "Teman"
+                    tab.setIcon(R.drawable.friends)
+                }
+                1 -> {
+                    tab.text = "Github"
+                    tab.setIcon(R.drawable.github)
+                }
+                2 -> {
+                    tab.text = "Profil"
+                    tab.setIcon(R.drawable.profile)
+                }
+            }
+        }.attach()
+    }
+}
